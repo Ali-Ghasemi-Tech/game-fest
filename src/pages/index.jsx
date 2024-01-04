68fc8074e5a1dec83bbe954e08a69a9797eeb9ff
TLB30 <thelastblade30@gmail.com>
2024-01-04 22:09:00 +0330

    add slider component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Link } from "react-router-dom";
+import ProductSlider from "../components/ProductSlider";
+import "../style/index.css";
 
 const index = () => {
   return (
     <>
       <Navbar />
-      <div className={"empty-cart"}>
-        <Link to="/shop">
-          <button className="add-button">Go To Shop</button>
-        </Link>
+      <div className="index-page">
+        <ProductSlider category={"mouse"} />
+        <div className="empty-cart">
+          <Link to="/shop">
+            <button className="add-button">Go To Shop</button>
+          </Link>
+        </div>
       </div>
     </>
   );
